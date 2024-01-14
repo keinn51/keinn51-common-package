cb973362b9d8714236de6f7e291bcc2befd987a9
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-01-14 15:27:55 +0900

    [chore] 불필요한 코드들 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,7 @@
-import Dropdown from "@/components/Dropdown";
-
-const ORDERBY_ARRAY = [
-  { key: "created", sortDesc: true, sort: "created", name: "최신등록순" },
-  { key: "orderCount", sortDesc: true, sort: "orderCount", name: "주문많은순" },
-  {
-    key: "reviewCount",
-    sortDesc: true,
-    sort: "reviewCount",
-    name: "리뷰많은순",
-  },
-  {
-    key: "reviewScore",
-    sortDesc: true,
-    sort: "reviewScore",
-    name: "평점높은순",
-  },
-  { key: "lowPrice", sortDesc: false, sort: "price", name: "낮은가격순" },
-  { key: "highPrice", sortDesc: true, sort: "price", name: "높은가격순" },
-];
-
 const Index = () => {
   return (
     <div>
-      <Dropdown
-        idx={ORDERBY_ARRAY.map((o) => o.key).indexOf("created")}
-        items={ORDERBY_ARRAY}
-      />
+      <h1>hello world</h1>
     </div>
   );
 };
--- src/styles/global/index.js
+++
@@ -1,7 +0,0 @@
-export * as FlexSC from "./Flex.styles";
-export * as GridSC from "./Grid.styles";
-export * as LineSC from "./Line.styles";
-export * as SpinSC from "./Spinner.styles";
-export * as TextSC from "./Text.styles";
-export * as FixedSC from "./Fixed.styles.js";
-export * as LayoutSC from "./Layout.styles";
