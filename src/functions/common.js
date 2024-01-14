327f4e96d12685f18a4c0127b9564aea8fc55a6e
keinn51 <soogood97@gmail.com>
2024-01-14 16:01:45 +0900

    [feat] base64 및 hex로 변환해주는 함수 4개 추가

--- src/functions/common.js
+++ src/functions/common.js
@@ -2,6 +2,39 @@ function add(a, b) {
   return a + b;
 }
 
+function stringToBase64(string) {
+  const base64String = btoa(string);
+  return base64String;
+}
+
+function stringToHex(string) {
+  let hexString = "";
+  for (let i = 0; i < string.length; i++) {
+    const hex = string.charCodeAt(i).toString(16);
+    hexString += hex;
+  }
+  return hexString;
+}
+
+function hexToString(hexString) {
+  let string = "";
+  for (let i = 0; i < hexString.length; i += 2) {
+    const hex = hexString.substr(i, 2);
+    const charCode = parseInt(hex, 16);
+    string += String.fromCharCode(charCode);
+  }
+  return string;
+}
+
+function base64ToString(base64String) {
+  const string = atob(base64String);
+  return string;
+}
+
 module.exports = {
   add,
+  stringToBase64,
+  stringToHex,
+  hexToString,
+  base64ToString,
 };
