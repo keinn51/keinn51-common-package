a5ebf2193d44f862c00d41bd80bdfa62ffeea35e
keinn51 <soogood97@gmail.com>
2024-01-14 16:06:00 +0900

    [refactor] root에 index.js 추가하여 바로 import 가능하도록

---
+++ index.js
@@ -0,0 +1,15 @@
+import {
+    add,
+    stringToBase64,
+    stringToHex,
+    hexToString,
+    base64ToString,
+} from '@/functions/common';
+
+module.exports = {
+    add,
+    stringToBase64,
+    stringToHex,
+    hexToString,
+    base64ToString,
+};
